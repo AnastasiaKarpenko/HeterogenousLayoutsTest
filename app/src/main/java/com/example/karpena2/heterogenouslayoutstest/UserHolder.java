38745b74eee0bdef3539eed17639151c681b07ba
Anastasia Karpenko <morganaloca@gmail.com>
2018-09-16 12:06:47 +0300

    Created UserHolder object and its layout

---
+++ app/src/main/java/com/example/karpena2/heterogenouslayoutstest/UserHolder.java
@@ -0,0 +1,34 @@
+package com.example.karpena2.heterogenouslayoutstest;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+public class UserHolder extends RecyclerView.ViewHolder {
+    private TextView mUserName;
+    private TextView mUserOrigin;
+
+    public UserHolder(@NonNull View itemView) {
+        super(itemView);
+
+        mUserName = itemView.findViewById(R.id.tv_userName);
+        mUserOrigin = itemView.findViewById(R.id.tv_userOrigin);
+    }
+
+    public TextView getUserName() {
+        return mUserName;
+    }
+
+    public void setUserName(TextView userName) {
+        mUserName = userName;
+    }
+
+    public TextView getUserOrigin() {
+        return mUserOrigin;
+    }
+
+    public void setUserOrigin(TextView userOrigin) {
+        mUserOrigin = userOrigin;
+    }
+}
