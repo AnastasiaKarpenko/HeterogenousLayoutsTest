1206be9f50a985356474aec8a30534a0613183b8
Anastasia Karpenko <morganaloca@gmail.com>
2018-09-16 11:27:44 +0300

    initial commit

---
+++ app/src/main/java/com/example/karpena2/heterogenouslayoutstest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.karpena2.heterogenouslayoutstest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private RecyclerView mRecyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+}
